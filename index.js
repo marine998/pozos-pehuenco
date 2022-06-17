6844c3c06efc2eecafcab159e483afc2f5ffefee
marine998 <luissamuelrochaarias@gmail.com>
2022-06-17 15:55:12 -0400

    FIX

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const mysql = require('mysql');
 
 // Initializations
 const app = express();
 
 // Settings
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3050);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
+
+// MySql
+const connection = mysql.createConnection({
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b0678c97880259',
+    password: '3ee47a8d',
+    database: 'heroku_e41262fc0f6ee1e'
+});
 
 // Middlewares
 app.use(morgan('dev'));
--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
-module.exports = {
+// module.exports = {
 
-    database: {
-        host: 'us-cdbr-east-05.cleardb.net',
-        user: 'b0678c97880259',
-        password: '3ee47a8d',
-        database: 'heroku_e41262fc0f6ee1e'
-    }
+//     database: {
+//         host: 'us-cdbr-east-05.cleardb.net',
+//         user: 'b0678c97880259',
+//         password: '3ee47a8d',
+//         database: 'heroku_e41262fc0f6ee1e'
+//     }
     
-};
+// };
