a8f4f330f11f78ff2abd0ede2dd99259d8f201dc
marine998 <luissamuelrochaarias@gmail.com>
2022-06-17 14:42:24 -0400

    test database

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
-module.exports = {
+// module.exports = {
 
-    database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'M@5012lsmr',
-        database: 'pozos_profundos_db'
-    }
+//     database: {
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'M@5012lsmr',
+//         database: 'pozos_profundos_db'
+//     }
     
-};
+// };
