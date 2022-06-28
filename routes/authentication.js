72226caa27ae15ea2fce51b6b04f2dd627695d09
Marine98 <56179039+marine998@users.noreply.github.com>
2022-06-27 21:49:59 -0400

    Revert "registro usuario"

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,37 +1,4 @@
 const express = require('express');
 const router = express.Router();
 
-const passport = require('passport');
-
-//SIGNUP
-router.get('/signup', (req, res) => {
-    res.render('Profile.html')
-
-});
-
-
-router.post('/signup', passport.authenticate('local.signup', {
-    successRedirect: '/profile',
-    failureRedirect: '/signup',
-    failureFlash: true
-}));
-
-// SINGIN
-router.get('/signin', (req, res) => {
-    res.render('/');
-});
-
-router.post('/signin', (req, res, next) => {
-
-    passport.authenticate('local.signin', {
-        successRedirect: '/profile',
-        failureRedirect: '/',
-        failureFlash: true
-    })(req,res, next);
-});
-
-router.get('/profile', (req, res) => {
-    res.render('/Profile');
-});
-
 module.exports = router;
